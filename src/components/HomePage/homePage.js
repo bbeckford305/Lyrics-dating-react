e042cb3a19a1ae62e8b25278acd1abc5557adb56
bbeckford305 <bridgette.beckford@gmail.com>
2021-06-04 00:20:36 -0700

    Deploy Image issue

--- src/components/HomePage/homePage.js
+++ src/components/HomePage/homePage.js
@@ -1,11 +1,11 @@
 
 import React from 'react'
-import videos from '../../public/videos/Dating_Intro_3.mp4'
+import video from '../public/videos/Dating_Intro_3.mp4'
 
 function HomePage () {
   return (
     <div className='homepage1' >
-      <video src={videos} autoPlay loop muted />
+      <video src={ video } autoPlay loop muted />
     </div>
   )
 }
